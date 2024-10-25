6161b47ff782b98bb0b412f4d3bfd7cb625190dc
zishanbd2000 <zishanbd2000@gmail.com>
2024-10-26 00:54:59 +0600

    3rd commit

--- js/home.js
+++ js/home.js
@@ -8,4 +8,23 @@ document
     //
     const pinNumber = document.getElementById("input-pin-number").value;
     console.log(pinNumber);
+
+    //
+
+    if (pinNumber === "1234") {
+      console.log("added money");
+    } else {
+      alert("failed to add money");
+    }
+    //
+    const balance = document.getElementById("acount-balance").innerText;
+    console.log(balance);
+    //
+
+    const addMoneyNumber = parseFloat(addMoney);
+    const balanceNumber = parseFloat(balance);
+    const newBalance = addMoneyNumber + balanceNumber;
+    console.log(newBalance);
+    //
+    document.getElementById("acount-balance").innerText = newBalance;
   });
