6aed05c877719cc5f59b62b7b8cb07ca2bf1869a
zishanbd2000 <zishanbd2000@gmail.com>
2024-10-26 00:01:05 +0600

    add money

--- js/home.js
+++ js/home.js
@@ -1,1 +1,11 @@
-console.log('home js connected')
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    //
+    const addMoney = document.getElementById("money-add").value;
+    console.log(addMoney);
+    //
+    const pinNumber = document.getElementById("input-pin-number").value;
+    console.log(pinNumber);
+  });
