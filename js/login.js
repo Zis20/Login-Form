f5c10c9664a679bf2cc7445a7e34ca333cd4504c
zishanbd2000 <zishanbd2000@gmail.com>
2024-10-25 23:24:55 +0600

    add money form added

--- js/login.js
+++ js/login.js
@@ -4,8 +4,19 @@ document
   .getElementById("button-login")
   .addEventListener("click", function (event) {
     event.preventDefault();
-
     console.log("login button click");
-    const phoneNumber = document.getElementById("phone-number");
-    console.log(phoneNumber.value);
+
+    const phoneNumber = document.getElementById("phone-number").value;
+    // console.log(phoneNumber.value);
+
+    const pinCode = document.getElementById("pin-code").value;
+    console.log(phoneNumber, pinCode);
+
+    if (phoneNumber === "5" && pinCode === "1234") {
+      console.log("you r logged in");
+      window.location.href = '/home.html'
+    } 
+    else {
+      alert("wrong phone number or pin");
+    }
   });
